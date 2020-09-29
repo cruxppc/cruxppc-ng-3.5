b5ded2707b8c3d127b989510c303f397de903af5
cruxppc <cruxppc@gmail.com>
2020-09-29 15:28:34 +0000

    Removed erroneous reference to cruxppc.org Fixes to iso rootfs creation Modified POST.sh such that it is independent of ports tree and prt-get package.

--- misc/post-installs/POST.sh
+++ misc/post-installs/POST.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # echo "running selected post-inst alls"
-prt-get isinst adwaita-icon-theme && (/bin/bash -v adwaita-icon-theme/post-install)
-prt-get isinst dbus && (/bin/bash -v dbus/post-install)
-prt-get isinst glib && (/bin/bash -v glib/post-install)
-prt-get isinst gtk3 && (/bin/bash -v gtk3/post-install)
-prt-get isinst libglade && (/bin/bash -v libglade/post-install)
-prt-get isinst libinput && (/bin/bash -v libinput/post-install)
-prt-get isinst librsvg-compat && (/bin/bash -v librsvg-compat/post-install)
-prt-get isinst shared-mime-info && (/bin/bash -v shared-mime-info/post-install)
-prt-get isinst xorg-font-misc-misc && (/bin/bash -v xorg-font-misc-misc/post-install)
+(pkginfo -i |grep '^adwaita-icon-theme ') &&  (/bin/bash -v adwaita-icon-theme/post-install)
+(pkginfo -i |grep '^dbus ')  &&  (/bin/bash -v dbus/post-install)
+(pkginfo -i |grep '^glib ')  &&  (/bin/bash -v glib/post-install)
+(pkginfo -i |grep '^gtk3 ')  &&  (/bin/bash -v gtk3/post-install)
+(pkginfo -i |grep '^libglade ')  &&  (/bin/bash -v libglade/post-install)
+(pkginfo -i |grep '^libinput ')  &&  (/bin/bash -v libinput/post-install)
+(pkginfo -i |grep '^librsvg-compat ')  &&  (/bin/bash -v librsvg-compat/post-install)
+(pkginfo -i |grep '^shared-mime-info ')  &&  (/bin/bash -v shared-mime-info/post-install)
+(pkginfo -i |grep '^xorg-font-misc-misc ')  &&  (/bin/bash -v xorg-font-misc-misc/post-install)
